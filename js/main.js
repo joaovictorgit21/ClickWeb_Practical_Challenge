489e46b37e5e047253164d0a8d87913e85f3dec7
joaovictorgit21 <joaovictorgit21@gmail.com>
2023-10-08 16:18:45 -0300

    Update - Practical Challenge

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll('.first-section, .second-section, .three-section');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.5 });
+
+sections.forEach(section => {
+    observer.observe(section);
+});
